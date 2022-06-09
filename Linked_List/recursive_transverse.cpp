fccf8e2bc31de353374fcf625931b51657846eff
adarshcode12 <97828274+adarshcode12@users.noreply.github.com>
2022-06-09 12:58:59 +0530

    Add files via upload

---
+++ Linked_List/recursive_transverse.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data=x;
+        next=NULL;
+    }
+};
+
+void transv(Node *head)
+{
+    if (head==NULL)
+    {
+        return;
+    }
+    cout<<head->data<<endl;
+    transv(head->next);
+}
+
+
+int main()
+{
+    Node *head=new Node(10);
+    head->next=new Node(20);
+    head->next->next=new Node(30);
+
+    transv(head);
+}
