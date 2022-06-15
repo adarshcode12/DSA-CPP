a921e9b0c08e098642c615abbec536a3e82abc45
adarshcode12 <97828274+adarshcode12@users.noreply.github.com>
2022-06-15 14:22:02 +0530

    Add files via upload

---
+++ Linked_List/insert_end_sll.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data=x;
+        next= NULL;
+    }
+};
+
+
+Node *insert_end(Node *head , int x)
+{
+    Node *temp = new Node(x);
+    if(head == NULL)
+    {
+        return temp;
+    }
+    Node *curr = head;
+    while(curr->next != NULL)
+    {
+        curr=curr->next;
+    }
+    curr->next=temp;
+    return head;
+
+}
+
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *temp1 = new Node(20);
+    Node *temp2 = new Node(30);
+
+    head->next = temp1;
+    temp1->next = temp2;
+
+    head=insert_end(head,50);
+    
+
+    Node *curr = head;
+    while(curr != NULL)
+    {
+        cout<<curr->data<<"->";
+        curr=curr->next;
+    }
+    return 0;
+}
