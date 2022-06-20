c009a412628109f0d94fe501e6b197941fe0488a
adarshcode12 <97828274+adarshcode12@users.noreply.github.com>
2022-06-20 23:00:02 +0530

    Add files via upload

---
+++ Linked_List/delete_end_sll.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data=x;
+        next= NULL;
+    }
+};
+
+
+Node *delete_end(Node *head)
+{
+    if (head == NULL)
+    {
+        return NULL;
+    }
+    if(head->next == NULL)
+    {
+        delete head;
+        return NULL;
+    }
+    else
+    {
+        Node *curr = head;
+        while(curr->next->next != NULL)
+        {
+            curr=curr->next;
+        }
+        delete (curr->next);
+        curr->next= NULL;
+        return head;
+    }
+}
+
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *temp1 = new Node(20);
+    Node *temp2 = new Node(30);
+
+    head->next = temp1;
+    temp1->next = temp2;
+
+    head=delete_end(head);
+    
+
+    Node *curr = head;
+    while(curr != NULL)
+    {
+        cout<<curr->data<<"->";
+        curr=curr->next;
+    }
+    return 0;
+}
